e4e5323fc8bf4a394630020d0300685ea97adb40
Carl Witt <wittcarl@informatik.hu-berlin.de>
2018-05-29 13:43:12 +0200

    Configuration for RMM8
    Increased slope range to [1,10] (variant calling bowtie-build has a slope of 7.5)
    Sampling per task type failure time mean uniformly from [0,1] and std dev from [0,0.5]. Sampling per task failure time from the normal distribution with these parameters.

--- bharathi/test/simulation/generator/AppGeneratorTest.java
+++ bharathi/test/simulation/generator/AppGeneratorTest.java
@@ -80,11 +80,16 @@ class AppGeneratorTest {
     @Test
     void generateWorkflows() throws Exception {
 
-        Path targetDir = Paths.get("results", "rmm7");
+        // TODO the distributions of the workflow generator (by Bharathi) do not exactly match the published numbers in Juve 2013 (FGCS)
+        // for instance the TmpltBank task type has a mean runtime of 20 seconds in the generator vs 500 sec in the publication
+        // also, some of the workflow's task types as published do not appear in the simulation
+
+        Path targetDir = Paths.get("results", "rmm8");
 
         // avoid mixing up commas and dots when converting floating points to string (german vs. english locales)
         Locale.setDefault(new Locale("EN_us")); //Locale.setDefault();//setDefault(new Locale());
 
+        Random random = new Random(1L);
         // workflow classes
         List<Class<? extends AbstractApplication>> applicationClasses = new LinkedList<>();
         applicationClasses.add(Cybershake.class);
@@ -96,7 +101,7 @@ class AppGeneratorTest {
         // tasks per workflow instance
         int[] workflowSizes = {
 //                100,
-                2500,
+//                2500,
                 5000,
 //                10000,
         };
@@ -104,15 +109,14 @@ class AppGeneratorTest {
         // number of workflow instances per configuration (class, num tasks)
         // since each workflow has randomized runtimes, memory consumptions, etc. we want
         // more than one instance per configuration
-        int numInstances = 20;
+        int numInstances = 200;
 
         // create a new workflow for each configuration (workflow type, num tasks, instance id)
         // write the dax output file
         // assemble workflow statistics
         for(Class<? extends AbstractApplication> appClass : applicationClasses){
-            for (Integer numTasks : workflowSizes) {
+            for (Integer workflowSize : workflowSizes) {
                 for (int instanceID = 0; instanceID < numInstances; instanceID++) {
-
 
                     WorkflowStatistics statistics;
                     AbstractApplication app;
@@ -122,7 +126,7 @@ class AppGeneratorTest {
                         // create a new Ligo/Cybershake/etc. object
                         app = appClass.newInstance();
                         // create the workflow topology and sample the runtimes
-                        app.generateWorkflow("-n", numTasks.toString());
+                        app.generateWorkflow("-n", workflowSize.toString());
 
                         // generate random memory model for each task type
                         for (String tasktype : app.getTasktypes()) {
@@ -134,9 +138,13 @@ class AppGeneratorTest {
                             double minFileSize = 10e3;
                             double maxMemConsumption = 64e9;
                             double linearTaskChance = 0.5;
-                            double minSlope = 0.5;
-                            double maxSlope = 2.0;
+                            double minSlope = 1;
+                            double maxSlope = 10.0;
                             LinearModel linearModel = LinearModel.randomMemoryModel(tasks.length, minFileSize, maxMemConsumption, linearTaskChance, minSlope, maxSlope);
+
+                            // random failure model
+                            double peakMemoryRelativeTimeMean = random.nextDouble();
+                            double peakMemoryRelativeTimeSd = random.nextDouble()/2.;
 
                             app.memoryModels.put(tasktype, linearModel);
 
@@ -145,13 +153,15 @@ class AppGeneratorTest {
 
                                 // add memory consumption both as XML element attribute and (as a compatibility hack, as a separate <argument> element)
                                 long peakMemoryConsumptionByte = (long) linearModel.getSamples()[1][i];
+                                // normally distributed and capped to range [0, 1]
+                                double peakMemoryRelativeTime = Math.min(1, Math.max(0, random.nextGaussian()*peakMemoryRelativeTimeSd+peakMemoryRelativeTimeMean));
 
                                 tasks[i].addAnnotation("peak_mem_bytes", Long.toString(peakMemoryConsumptionByte));
-                                tasks[i].addArgument(new PseudoText(String.format("peak_mem_bytes=%d,peak_memory_relative_time=%.3f", peakMemoryConsumptionByte, 0.5)));
+                                tasks[i].addArgument(new PseudoText(String.format("peak_mem_bytes=%d,peak_memory_relative_time=%.3f", peakMemoryConsumptionByte, peakMemoryRelativeTime)));
 
                                 double[] filesizes = linearModel.getSamples()[0];
                                 if (tasks[i].getInputs().size() == 0) {
-                                    System.err.printf("AppGeneratorTest.generateWorkflows: %s (%s.n.%s.%s.dax) has zero input files to distribute input size to%n", tasktype, appClass.getSimpleName(), numTasks, instanceID);
+                                    System.err.printf("AppGeneratorTest.generateWorkflows: %s (%s.n.%s.%s.dax) has zero input files to distribute input size to%n", tasktype, appClass.getSimpleName(), workflowSize, instanceID);
                                     continue;
                                 }
                                 long averageInputSize = ((long) filesizes[i]) / tasks[i].getInputs().size();
@@ -173,7 +183,7 @@ class AppGeneratorTest {
 
                     for(String tasktype : app.getTasktypes()){
                         DescriptiveStatistics memory = statistics.memoryUsagesPerTaskType.get(tasktype);
-                        System.out.printf("%s,%s,%s,%s,%s,%.2f,%.2f %n", app.getClass().getSimpleName(), numTasks, instanceID, tasktype, statistics.numberOfTasksPerTaskType.get(tasktype), memory.getMean()/1e9, memory.getStandardDeviation()/1e9);
+                        System.out.printf("%s,%s,%s,%s,%s,%.2f,%.2f %n", app.getClass().getSimpleName(), workflowSize, instanceID, tasktype, statistics.numberOfTasksPerTaskType.get(tasktype), memory.getMean()/1e9, memory.getStandardDeviation()/1e9);
 //            System.out.println("numberOfTasksPerTaskType = " + statistics.numberOfTasksPerTaskType.get(tasktype));
 //                        System.out.println("inputSizes = " + descriptiveStats(statistics.inputSizesPerTaskType.get(tasktype)));
 //                        System.out.println("peakMem = " + descriptiveStats(statistics.memoryUsagesPerTaskType.get(tasktype)));
